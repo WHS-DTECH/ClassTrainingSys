47d3ee552029e47d91ec3dfdceb34215cd264806
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2025-12-24 18:59:06 +1300

    update all

--- migrations/versions/6152ba9fa1f8_add_lesson_model_and_update_section_.py
+++ migrations/versions/6152ba9fa1f8_add_lesson_model_and_update_section_.py
@@ -48,7 +48,7 @@ def upgrade():
 
     # 4. Add lesson_id as nullable to sections
     with op.batch_alter_table('sections', schema=None) as batch_op:
-        batch_op.add_column(sa.Column('lesson_id', sa.Integer(), nullable=True))
+        # batch_op.add_column(sa.Column('lesson_id', sa.Integer(), nullable=True))  # Skipped: column already exists
         # batch_op.drop_constraint(batch_op.f('fk_sections_course'), type_='foreignkey')  # Constraint does not exist
         batch_op.create_foreign_key(None, 'lessons', ['lesson_id'], ['id'])
         # batch_op.drop_column('course_id')  # Column does not exist
