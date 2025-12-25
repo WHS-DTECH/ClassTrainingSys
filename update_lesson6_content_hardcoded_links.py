f9b320997479cdee3f96e1e64d32a6598bea3d21
Vanessa Pringle <vanessapringle@westlandhigh.school.nz>
2025-12-25 16:29:52 +1300

    Lesson 2 sections

--- update_lesson6_content_hardcoded_links.py
+++ update_lesson6_content_hardcoded_links.py
@@ -13,9 +13,9 @@ with app.app_context():
 <p><b>Instructor:</b> Vanessa Pringle (HOLA Technology)</p>
 <h2>Course Content</h2>
 <ol>
-  <li><a href="/lessons/sections/9">Section 1: Debugging Mindset</a></li>
-  <li><a href="/lessons/sections/10">Section 2: Common Debugging Techniques</a></li>
-  <li><a href="/lessons/sections/11">Section 3: Persistence and Problem-Solving</a></li>
+  <li><a href="/lessons/sections/9">Section 1: Python Debugging – Best Practices</a></li>
+  <li><a href="/lessons/sections/10">Section 2: JavaScript Debugging – Best Practices</a></li>
+  <li><a href="/lessons/sections/11">Section 3: HTML and CSS Debugging – Best Practices</a></li>
   <li><a href="/lessons/sections/12">Section 4: Debugging in Practice</a></li>
 </ol>'''
         db.session.commit()
